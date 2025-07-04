d0f90f1a774ed3fe15de5642f68526f254dcb2f0
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-04 17:03:39 +0530

    initial files

---
+++ 02passwordGenerator/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numbAllowed,setNumbAllowed] = useState(false)
+  const [charAllowed,setCharAllowed] = useState(false) 
+
+  return (
+    <>
+    <div className='flex justify-center items-center min-h-screen  bg-black-800'>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-6 pb-8 font-medium text-orange-700'>
+        <h1>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-2'>
+        <input type="text" 
+        className='outline-none w-full px-3 '
+        placeholder='enter text'
+         
+        />
+        <button className='px-2 bg-blue-500 text-white py-1'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2 '>
+          <div className=' flex  items-center gap-x-1'>
+            <input type="range" 
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            />
+            <label>length</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" 
+            
+            />
+            <label>Number</label>
+          </div>
+          <div className='flex items-center gap-x-2'>
+            <input type="checkbox" 
+            
+            />
+            <label>Character</label>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
