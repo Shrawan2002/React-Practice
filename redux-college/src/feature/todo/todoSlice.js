c735b300127a810f230f68083f5261a3f1263444
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-17 16:31:26 +0530

    initial files

--- redux-college/src/feature/todo/todoSlice.js
+++ redux-college/src/feature/todo/todoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = {
     todos:[{id:"abc", task: "demo-task", isDone: false}]
@@ -10,6 +10,21 @@ export const todoSlice = createSlice({
     initialState,
 
     reducers: {
-        
+        addTodo: (state,action) =>{
+            let newTodo = {
+               id: nanoid(),
+               task: action.payload,
+               isDone: false
+            }
+            state.todos.push(newTodo);
+        },
+
+        deleteTodo : (state, action)=>{
+            state.todos = state.todos.filter((todo)=> todo.id != action.payload);
+        },
+
+        marksAsDone : (state, action)=>{
+            
+        }
     }
 })
